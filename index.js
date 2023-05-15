894f69312e582a167703cf9a74a34e274647afd0
Brady Maradiaga <chiefmonchyy@gmail.com>
2023-05-14 21:04:38 -0400

    completed looping code along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const thankPeople = [];
+
+function writeCards(people, event) {
+    for (let i = 0; i < people.length; i++) {
+
+        thankPeople.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thankPeople;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
